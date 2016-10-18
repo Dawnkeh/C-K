a2a112fae765d633849985c1239997434259a28f
Dawnkeh <lilsk8rdude00@gmail.com>
2016-10-18 16:02:52 -0300

    Update KillSwitch.bat

--- KillSwitch.bat
+++ KillSwitch.bat
@@ -1,6 +1,6 @@
 @echo off
 :1
-del C:\CK\Untitled-1.bat
-del C:\CK\Untitled-2.bat
-del C:\CK\Untitled-3.bat
+del C:\CK\Untitled-1.bat  -t
+del C:\CK\Untitled-2.bat  -t
+del C:\CK\Untitled-3.bat  -t
 exit
