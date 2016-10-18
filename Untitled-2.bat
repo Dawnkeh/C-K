2d8778c89f2741a6eb6875446ac18e8f627e112a
Dawnkeh <lilsk8rdude00@gmail.com>
2016-10-18 16:01:40 -0300

    Update Untitled-2.bat

--- Untitled-2.bat
+++ Untitled-2.bat
@@ -3,7 +3,7 @@ color 0b
 
 :1
 tree
-start c:/desktop/CK/Untitled-2.bat
-start c:/desktop/CK/Untitled-3.bat
-start c:/desktop/CK/Untitled-1.bat
+start C:\CK\Untitled-2.bat
+start C:\CK\Untitled-3.bat
+start C:\CK\Untitled-4.bat
 goto 1
